df8b5dcfbbd80a623a85cd58cbfee9b0272870fa
ryder247 <gheddyboi@gmail.com>
2016-06-12 13:57:19 +0000

    fixed the favorite

--- m-website/music/urls.py
+++ m-website/music/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     #/music/ ---> main page
     url(r'^$', views.index, name='index'),
 
-    #/music/42/ ---> details page
+    #/music/<album_id>/ ---> details page
     url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
-    #/music/42/favorite ---> favorite page
+    #/music/<album_id>/favorite ---> favorite page
     url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- m-website/music/views.py
+++ m-website/music/views.py
@@ -14,11 +14,12 @@ def favorite(request, album_id):
     try:
         selected_song = album.song_set.get(pk=request.POST['song'])
     except (KeyError, Song.DoesNotExist):
-        return render( request, 'music/detail.html', {
-            'album': album,
-            'error_message':"You did not select a valid song",
+        return render(request, 'music/detail.html', {
+            'album':album,
+            'error_message':"You did not select a valid Song.",
         })
     else:
         selected_song.is_favorite = True
         selected_song.save()
         return render( request, 'music/detail.html', {'album': album})
+
