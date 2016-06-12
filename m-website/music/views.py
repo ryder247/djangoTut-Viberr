5986e14bd865afe0f1446c46af19bd34f3476ced
ryder247 <gheddyboi@gmail.com>
2016-06-12 16:29:59 +0000

    added bootstrap and started styling the home page
    added the navbar to the index page

--- m-website/music/views.py
+++ m-website/music/views.py
@@ -16,10 +16,9 @@ def favorite(request, album_id):
     except (KeyError, Song.DoesNotExist):
         return render(request, 'music/detail.html', {
             'album':album,
-            'error_message':"You did not select a valid Song.",
+            'error_message':"You did not select a valid song.",
         })
     else:
         selected_song.is_favorite = True
         selected_song.save()
         return render( request, 'music/detail.html', {'album': album})
-
